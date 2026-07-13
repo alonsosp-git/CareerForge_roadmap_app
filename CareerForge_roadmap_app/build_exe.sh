8b5e6f1eb1b501a37b03b92304a1db99697beffc
alonsosp-git <alonsosp@gmail.com>
2026-07-13 00:58:10 -0600

    Delete CareerForge_roadmap_app directory

--- CareerForge_roadmap_app/build_exe.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 -m pip install --upgrade pip
-python3 -m pip install flask pyinstaller
-python3 -m PyInstaller --onefile --name CareerForge --add-data "templates:templates" --collect-all flask launcher.py
-echo "Done -> dist/CareerForge"
